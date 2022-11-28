37c646dcea8d53fcffa17c8e0bdd06e4d428eabf
= <=>
2022-11-28 18:37:26 +0000

    done with basics

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -11,8 +11,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("administrator/", admin.site.urls),
-    path("api/", include("api.urls", namespace="api")),
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", include("api.urls", namespace="api")),
+    # path("", TemplateView.as_view(template_name="index.html")),
 ]
 
 
